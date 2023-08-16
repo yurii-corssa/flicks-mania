b4a06d66d39a6176f37db51abdd1d979ae4c5f0c
yurii-corssa <122684054+yurii-corssa@users.noreply.github.com>
2023-08-16 19:28:50 +0000

    Deploying to gh-pages from @ yurii-corssa/goit-react-hw-05-movies@7d4953a72ee533fcd7a14f2d04f80029bee54699 🚀

---
+++ static/js/247.2c5990f0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[247],{247:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Cast"})}}}]);
+//# sourceMappingURL=247.2c5990f0.chunk.js.map
