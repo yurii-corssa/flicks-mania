caf6cac4eafdd96123cf04a4b028b14cffb97659
Corssa <122684054+yurii-corssa@users.noreply.github.com>
2024-01-21 11:08:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/flicks-mania">
       <App />
     </BrowserRouter>
   </React.StrictMode>
